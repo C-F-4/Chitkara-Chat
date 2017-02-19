25935fa03053523569b9a579d8b6450dff824bac
Co_f <colinfinn4@gmail.com>
2017-02-19 10:38:58 +0530

    Base Structure Created

---
+++ js/index-scripts.js
@@ -0,0 +1,12 @@
+/*(function() {
+    var pagebytes = $('html').html().length;
+    var kbytes = pagebytes / 1024;
+    $('#loader-percent').innerHTML = kbytes;
+})();*/
+
+$(document).ready(function() {
+  // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 1000);
+});

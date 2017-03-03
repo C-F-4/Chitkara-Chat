6ddb891c8f1d89ab1bf001a22c84f2d4c5e12fcd
Co_f <colinfinn4@gmail.com>
2017-03-03 23:30:07 +0530

    Front-End for the LandingPage with Forms Completed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Chitkara-Chat
 Chatting Application created for Easy Communication inside a University or a School or any other for of Educational System
+
+# Issues
+Using Tab key can easily hover over form elements. Since the Slides are created using Radio. It also hovers through that.
+Initially the default arrow binding doesn't work on nav. But only after you select another Radio/label.
--- js/form.js
+++ js/form.js
@@ -1,16 +1,38 @@
-var myApp1 = angular.module("myApp1", []);
-myApp1.controller("RegisterCtrl", function ($scope) {
+var indexApp = angular.module("indexApp", ["signupApp", "loginApp", "contactApp"]);
 
+var signupApp = angular.module("signupApp", []);
+signupApp.controller("RegisterCtrl", function ($scope) {
+    $scope.user = {};
+    $scope.validate = function(user) {
+        console.log($scope.user);
+        // check if University ID or Email is already in the database
+    }
 });
 
-var myApp2 = angular.module("myApp2", []);
-myApp2.controller("LoginCtrl", function ($scope) {
-
+var loginApp = angular.module("loginApp", []);
+loginApp.controller("LoginCtrl", function ($scope) {
+    $scope.master = {};
+    $scope.validate = function(master) {
+        console.log($scope.master);
+        // Test if Email ID and password matches as in the database
+    }
 });
 
-var myApp3 = angular.module("myApp3", []);
-myApp3.controller("ContactCtrl", function ($scope) {
-
+var contactApp = angular.module("contactApp", []);
+contactApp.controller("ContactCtrl", function ($scope) {
+    $scope.guest = {};
+    $scope.validate = function(guest) {
+        console.log($scope.guest);
+        // Mailer function
+    }
 });
 
-var myApp = angular.module("myApp", ["myApp1", "myApp2", "myApp3"]);
+var sectionSignup = document.getElementById("signup-app");
+var sectionLogin = document.getElementById("login-app");
+var sectionContact = document.getElementById("contact-app");
+
+angular.element(document).ready(function() {
+   angular.bootstrap(sectionSignup, ['signupApp']);
+   angular.bootstrap(sectionLogin, ['loginApp']);
+   angular.bootstrap(sectionContact, ['contactApp']);
+});

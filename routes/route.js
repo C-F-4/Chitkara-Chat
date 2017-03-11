2cac2acdbce65ca5b93e25ed7094105d42b8b0ef
Co_f <colinfinn4@gmail.com>
2017-03-11 17:52:33 +0530

    Moved db_script to appropriate folder

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,7 @@ var path = require('path');
 var router = express.Router();
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
+console.log(path.join(__dirname, '../public'));
 
 router.get('/', function(req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, '../public') });
